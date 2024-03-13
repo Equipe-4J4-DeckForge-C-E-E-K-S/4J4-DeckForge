ca8a3e9ea7b9382e123902a99159f0c8db312474
EricNguyen0 <e1446191@cmaisonneuve.qc.ca>
2024-03-13 13:41:42 -0400

    ajout menu aide
    avoir si le volume slider script marche maison

--- Assets/script/VolumeSliderScript.cs
+++ Assets/script/VolumeSliderScript.cs
@@ -5,11 +5,19 @@ using UnityEngine.Audio;
 
 public class VolumeSliderScript : MonoBehaviour
 {
+    public AudioMixer audioMixer;
 
-public AudioMixer audioMixer;
+    public void SetVolume(float volume)
+    {
+        // Ensure volume stays within a reasonable range (optional)
+        volume = Mathf.Clamp(volume, 0.0001f, 1.0f);
 
- public void SetVolume(float volume)
- {
-        audioMixer.SetFloat("Volume", volume);
- }
+        // Set the volume in the AudioMixer
+        audioMixer.SetFloat("Volume", Mathf.Log10(volume) * 20);
+
+        // Log the current volume level to the console
+        float currentVolume;
+        audioMixer.GetFloat("Volume", out currentVolume);
+        Debug.Log("Current Volume: " + currentVolume);
+    }
 }
