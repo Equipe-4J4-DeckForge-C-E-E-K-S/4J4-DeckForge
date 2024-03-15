8239e519651e2cfa1e9d5cdc8f1b776929a2201b
EricNguyen0 <e1446191@cmaisonneuve.qc.ca>
2024-03-14 23:54:01 -0400

    mixer music pas parfait mais ca va faire laffaire

--- Assets/script/VolumeSliderScript.cs
+++ Assets/script/VolumeSliderScript.cs
@@ -9,15 +9,6 @@ public class VolumeSliderScript : MonoBehaviour
 
     public void SetVolume(float volume)
     {
-        // Ensure volume stays within a reasonable range (optional)
-        volume = Mathf.Clamp(volume, 0.0001f, 1.0f);
-
-        // Set the volume in the AudioMixer
-        audioMixer.SetFloat("Volume", Mathf.Log10(volume) * 20);
-
-        // Log the current volume level to the console
-        float currentVolume;
-        audioMixer.GetFloat("Volume", out currentVolume);
-        Debug.Log("Current Volume: " + currentVolume);
+       audioMixer.SetFloat("volume", volume);
     }
 }
