6785e0ec20b64367427ecfbbede3fa84146f689c
BecotteEmile <emilebecotte@gmail.com>
2024-03-18 14:18:58 -0400

    librairie teste et fonctionnelle

--- Assets/scriptsEmile/testCartes/librairieDeck.cs
+++ Assets/scriptsEmile/testCartes/librairieDeck.cs
@@ -3,44 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class comportementCartes : MonoBehaviour
+public class librairieDeck : MonoBehaviour
 {
-    public GameObject[] ligma;
-    static GameObject[] balls;
-    public GameObject[] ballsDebug;
-    //public GameObject[] nouveauDeck;
-    public int index2;
-    public GameObject nouvelleCarte;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //ballsDebug = new GameObject[10];
-        balls = ballsDebug;
-        ligma = balls;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        balls = ballsDebug;
-        
-    }
-
-    public void setup()
-    {
-        //ligma = ajouterCarte(ligma, nouvelleCarte);
-        ligma = enleverCarte(ligma, index2);
-    }
-
-
     public GameObject[] ajouterCarte(GameObject[] deckUtilise, GameObject carteAAjouter)
     {
         GameObject[] nouveauDeck = new GameObject[deckUtilise.Length + 1];
         int index = 0;
         foreach (var carte in nouveauDeck)
         {
-            if (index < ligma.Length)
+            if (index < deckUtilise.Length)
             {
                 Debug.Log(index);
                 nouveauDeck[index] = deckUtilise[index];
