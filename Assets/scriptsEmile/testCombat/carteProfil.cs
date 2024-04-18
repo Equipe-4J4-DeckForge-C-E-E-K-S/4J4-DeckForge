79ba0e649173ff6e27100e7197721b32eed6260a
BecotteEmile <emilebecotte@gmail.com>
2024-04-18 19:19:31 -0400

    retour à la normale?

---
+++ Assets/scriptsEmile/testCombat/carteProfil.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carteProfil : MonoBehaviour
+{
+    public bool attaquer;
+    public bool bloquer;
+    public bool soigner;
+}
