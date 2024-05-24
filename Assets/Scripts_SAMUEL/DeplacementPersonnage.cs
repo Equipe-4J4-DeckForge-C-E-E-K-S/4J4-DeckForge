bc687f5cdfa3552de3ca9798e43e2bac509b59f1
Samuel Giroux <s_giroux@icloud.com>
2024-05-24 14:08:01 -0400

    Création du script de déplacement du joueur dans la section du marché

---
+++ Assets/Scripts_SAMUEL/DeplacementPersonnage.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeplacementPersonnage : MonoBehaviour
+{
+    public float vitesseMouvement;
+
+    public Rigidbody2D rigidBody;
+
+    private Vector2 directionDeplacement;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Rigidbody du personnage
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*vitesseX = Input.GetAxisRaw("Horizontal") + vitesseMouvement;
+        vitesseY = Input.GetAxisRaw("Vertical") + vitesseMouvement;
+        rigidBody.velocity = new Vector2(vitesseX, vitesseY);*/
+
+        float deplacementX = Input.GetAxisRaw("Horizontal");
+        float deplacementY = Input.GetAxisRaw("Vertical");
+
+        directionDeplacement = new Vector2(deplacementX, deplacementY);
+    }
+
+    void FixedUpdate()
+    {
+        Deplacement();
+    }
+
+    void Deplacement() {
+        rigidBody.velocity = new Vector2(directionDeplacement.x * vitesseMouvement, directionDeplacement.y * vitesseMouvement);
+    }
+}
