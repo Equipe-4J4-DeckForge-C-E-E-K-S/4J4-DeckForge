8a1f373d6a300ed2828ee328fd45bcb64ea76bcd
EricNguyen0 <e1446191@cmaisonneuve.qc.ca>
2024-06-03 22:44:57 -0400

    build debug texture code

--- Assets/scriptsEmile/testCombat/carteProfil.cs
+++ Assets/scriptsEmile/testCombat/carteProfil.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-using static UnityEditor.PlayerSettings;
+// using static UnityEditor.PlayerSettings;
 
 public class carteProfil : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
